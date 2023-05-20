e5d9790e8cd8cbdccd7c200a787dc403e1d5ef4d
Dmitry Pronin <dpronin@yandex.ru>
2023-05-19 20:05:17 -0700

    inittto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { Component, createSignal } from 'solid-js';
+
+const App: Component = () => {
+
+  return (
+    <p class="text-4xl text-green-700 text-center py-20">Hello wrewindi-css!</p>
+  );
+};
+
+export default App;
